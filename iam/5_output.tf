74d6bbc6e032c394a3f2f53a39ad82d5be431ad2
erioluwa66 <erioluwa66@gmail.com>
2023-11-03 22:44:27 -0400

    fix: output modified and Readme added

---
+++ iam/5_output.tf
@@ -0,0 +1,9 @@
+
+output "node_role" {
+  value = aws_iam_role.demo.arn
+
+}
+
+output "demo_role" {
+  value = aws_iam_role.nodes.name
+}
--- vpc/8_output.tf
+++ vpc/8_output.tf
@@ -1,18 +1,10 @@
 output "private" {
-    value = aws_subnet.private.*.id
-  
+  value = aws_subnet.private.*.id
+
 }
 
 output "Public" {
-    value = aws_subnet.public.*.id
-  
+  value = aws_subnet.public.*.id
+
 }
 
-output "node_role" {
-    value = aws_iam_role.demo.arn
-  
-}
-
-output "demo_role" {
-    value = aws_iam_role.nodes.name
-  }
