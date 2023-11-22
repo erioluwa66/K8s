f8d3f126bb03fa65586f0a61cfd653ac7b415b6f
erioluwa66 <erioluwa66@gmail.com>
2023-11-22 12:22:54 -0500

    oidc fixed

--- 5.oidc_test/data.tf
+++ 5.oidc_test/data.tf
@@ -1,29 +1,30 @@
 data "aws_eks_cluster" "my-cluster" {
-name = "demo"
-  }
+  name = "demo"
+}
 
-  data "tls_certificate" "eks" {
-    url = data.aws_eks_cluster.my-cluster.identity[0].oidc[0].issuer    
-  }
+data "tls_certificate" "eks" {
+  url = data.aws_eks_cluster.my-cluster.identity[0].oidc[0].issuer
+}
 
+#IAM POLICY FOR OIDC PROVIDER
 
-  #IAM policy for OIDC provider
+data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
 
-  data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
-    statement {
-      actions = ["sts:AssumeRoleNewWebIdentity"]
-     effect = "Allow"
-
-     condition {
-       test = "StringEquals"
-       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-       values = ["system:serviceaccount:default:aws-test"]
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:default:aws-test"]
     }
-    
+
+
     principals {
       identifiers = [aws_iam_openid_connect_provider.eks.arn]
-     type = "Federated"
+      type        = "Federated"
     }
-    }
-
   }
+
+
+}
